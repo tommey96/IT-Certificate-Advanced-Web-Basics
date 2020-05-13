6dd97db66b8025125bf916bc660a4b9fa819b6a6
tommey96 <t-l.meyer@mailbox.org>
2020-05-13 21:34:06 +0200

    3 files modified

--- index.js
+++ index.js
@@ -3,7 +3,25 @@ import './style.css';
 
 // Write Javascript code!
 
+var kontaktbereich = document.getElementById('kontakt-bereich')
+
 var submitButton = document.getElementById('kontaktformular-submit')
 submitButton.addEventListener('click', function(event){
+var mail = document.getElementById('kontaktformular-mail')
+var sinfonie = document.getElementById('kontaktformular-sinfonie')
+var inhalt = document.getElementById('kontaktformular-inhalt')
+console.log(inhalt)
+
+var kontakt = ''
+
+kontakt += '<section class="kontakt">'
+
+kontakt += '<span>' + mail.value + ' - Lieblingssinfonie: ' + sinfonie.value + '</span>'
+
+kontakt += '<p>' + inhalt.value + '</p>'
+
+kontakt += '</section>'
+
+kontaktbereich.innerHTML += kontakt
 
 })
